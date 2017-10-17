b5a1b7343b90071eb262bb6ce3efafba029d8f66
Jeff Shomali <jeffshomali@gmail.com>
2017-10-17 14:05:17 -0700

    Working with Tab Bar navigation

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,29 @@
 import React, { Component } from "react";
 import { Text, View } from "react-native";
-
 import { Container, Content } from "native-base";
+import { Router, Scene } from "react-native-router-flux";
+
 import AppHeader from "./AppHeader";
 import AppFooter from "./AppFooter";
-import Home from "./Home";
+import Maps from "./pages/Maps";
+import Stations from "./pages/Stations";
+import Favorites from "./pages/Favorites";
+import More from "./pages/More";
 
-class App extends Component {
+export default class App extends Component {
   render() {
     return (
       <Container>
         <AppHeader />
-        <Home />
+
+        <Router>
+          <Scene key="root">
+            <Scene key="station" component={Stations} title="Station" />
+          </Scene>
+        </Router>
+
         <AppFooter />
       </Container>
     );
   }
 }
-
-export default App;
